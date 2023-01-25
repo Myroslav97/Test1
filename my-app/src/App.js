fdaab84525f84ad75b4a418f9f1fbf78128122d0
Myroslav Pihur <muroslav321@gmail.com>
2023-01-26 01:08:34 +0200

    final

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const HelloWorld = () => {
+  return (
+    <div className='hello'>
+      <h1>'Hello World'</h1>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <HelloWorld/>
+  );
+}
+
+export default App;
